2ed8c3dea2036efe6479bf874bcb0a0a387a3b28
Thomas Steinholz <tsteinholz@yahoo.com>
2016-04-16 12:52:59 -0400

    basic movment

--- Assets/Scripts/Player Controller/Controller.cs
+++ Assets/Scripts/Player Controller/Controller.cs
@@ -1,44 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-/**
-goals: 
-    [] move player with WASD
-    [] rotate player (always looking at mouse)
-    [] handle physics
-**/
-
 public class Controller : MonoBehaviour {
 
     [Range(0, 20)] public float WalkingSpeed = 6f;
-    [Range(0, 20)] public float RunningSpeed = 9f;
 
-    public enum State {
-        Idle,
-        Walking,
-        Running,
-        Attacking
-    }
-
-    private State _characterState;
     private Rigidbody2D _playerRigidBody;
-    private Vector2 _movement;
+    private Vector2 _veloctiy;
 
-    void Awake()
-    {
-        if (GetComponent<Rigidbody2D>() != null)
-            _playerRigidBody = GetComponent<Rigidbody2D>();
-        else
-            Debug.Log("No RigidBody Found!");
-       
-        _characterState = State.Idle;
+    void Awake() {
+        Debug.Assert(GetComponent<Rigidbody2D>() != null);
+        _playerRigidBody = GetComponent<Rigidbody2D>();
     }
 
     void Start () {
-	
-	}
-	
-	void FixedUpdate () {
-	
-	}
+
+    }
+
+    void FixedUpdate () {
+        _veloctiy.Set(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        _veloctiy = _veloctiy.normalized * WalkingSpeed * Time.deltaTime;
+        _playerRigidBody.MovePosition(_playerRigidBody.position + _veloctiy);
+    }
 }
