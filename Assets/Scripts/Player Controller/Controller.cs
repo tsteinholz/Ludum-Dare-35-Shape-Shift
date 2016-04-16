025541baba32be25e8f2803ac1d119b84ba71beb
Thomas Steinholz <tsteinholz@yahoo.com>
2016-04-16 13:24:27 -0400

    optimization

--- Assets/Scripts/Player Controller/Controller.cs
+++ Assets/Scripts/Player Controller/Controller.cs
@@ -6,20 +6,16 @@ public class Controller : MonoBehaviour {
     [Range(0, 20)] public float WalkingSpeed = 6f;
 
     private Rigidbody2D _playerRigidBody;
-    private Vector2 _veloctiy;
 
     void Awake() {
         Debug.Assert(GetComponent<Rigidbody2D>() != null);
         _playerRigidBody = GetComponent<Rigidbody2D>();
     }
 
-    void Start () {
-
-    }
-
     void FixedUpdate () {
-        _veloctiy.Set(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        _veloctiy = _veloctiy.normalized * WalkingSpeed * Time.deltaTime;
-        _playerRigidBody.MovePosition(_playerRigidBody.position + _veloctiy);
+        _playerRigidBody.velocity = new Vector2(
+            Mathf.Lerp(0, Input.GetAxis("Horizontal") * WalkingSpeed, 0.8f),
+            Mathf.Lerp(0, Input.GetAxis("Vertical") * WalkingSpeed, 0.8f)
+            );
     }
 }
