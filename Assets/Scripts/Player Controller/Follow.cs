8732291da0a1a6fc06db09b56bcdf9cc3113eb36
Thomas Steinholz <tsteinholz@yahoo.com>
2016-04-16 13:35:02 -0400

    cam controler

---
+++ Assets/Scripts/Player Controller/Follow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+    public Transform player;
+    [Range(1, 20)] public float smoothing = 10f;
+
+    Vector3 offset, normalOffset;
+
+    void Start()
+    {
+        offset = transform.position - player.position;
+        normalOffset = offset;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 playerCamPos = player.position + offset;
+        transform.position = Vector3.Lerp(transform.position, playerCamPos, smoothing * Time.deltaTime);
+    }
+}
